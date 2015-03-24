e3147329f99ba9acdeee95f29917ef8646a5d6e9
Hadzidy <hadzidy@grupow.com>
2015-03-24 08:58:15 -0600

    ususario desconetado

--- app.js
+++ app.js
@@ -11,20 +11,36 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/assets'));
 
 
-
+var usernames= {};
 
 io.on('connection', function(socket) {
 	console.log("alguien se conecto");
+	var addedUser = false;
+
 	socket.on('add connection', function(name) {
+		if(usernames[name]){
+			addedUser= false;
+			socket.emit("already exist");
+		}else{
+		addedUser= true;
+		usernames[name]= name;
+		socket.user= name;
 		console.log("Se conecto " + name);
 		socket.broadcast.emit("add user", name);
 		socket.emit("connect user", true);
+		}
 	});
 	socket.on('message', function(message) {
-	  console.log("el dice" , message);
-	  socket.broadcast.emit("send message", message)
+	  console.log("nuevo mensaje");
+	  var data = {username: socket.user,
+     			 message: message};
+
+	  socket.broadcast.emit("send message", data)
 	});
+
 	socket.on('disconnect', function() {
+		socket.broadcast.emit("user desconected", socket.user);
+		if (addedUser) delete usernames[socket.user];
 	  console.log("alguien se desconecto");
 	});
 });
